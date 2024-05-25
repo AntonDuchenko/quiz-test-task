bc989579165b31e9bcab4362ced111986ccaddbf
Anton Duchenko <deragandi0@gmail.com>
2024-05-25 09:54:55 +0300

    add toast styles

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 import { useAppDispatch } from "./app/reduxHooks";
 import * as quizesSlice from "./features/quizesSlice";
 import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const dispatch = useAppDispatch();
